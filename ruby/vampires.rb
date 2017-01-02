7c94b39a4e978cc86068448c5458c4d0e587cc44
Christopher Frazzini <cfrazzin@indiana.edu>
2017-01-01 23:49:09 -0600

    made initial updates to vampires

---
+++ ruby/vampires.rb
@@ -0,0 +1,20 @@
+puts "what is your name?"
+name = gets.chomp
+
+puts "how old are you?"
+age = gets.chomp
+
+puts "what year were you born?"
+year_born = gets.chomp
+
+puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+garlic_bread = gets.chomp
+
+puts "Would you like to enroll in the company’s health insurance?"
+health_insurance = gets.chomp 
+
+
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
