6bba4a4995e405913dd184517d8086f838785e19
Christopher Frazzini <cfrazzin@indiana.edu>
2017-01-04 01:49:29 -0600

    finished 4.4, now reviewing

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,9 @@
+puts "how many employees will be processed?"
+employees_processed = gets.chomp.to_i
+surveys_taken = 0 
+
+until surveys_taken == employees_processed
+
 puts "what is your name?"
 name = gets.chomp
 
@@ -11,37 +17,76 @@ current_year = 2016
 puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
 garlic_bread = gets.chomp
 
-if garlic_bread == "yes"
-  garlic_bread = true
-elsif garlic_bread == "no"
-  garlic_bread = false
-end 
-    
-
 puts "Would you like to enroll in the company’s health insurance? (yes/no)"
 health_insurance = gets.chomp 
 
-if health_insurance == "yes"
-  health_insurance = true
-elsif health_insurance == "no"
-  health_insurance = false
+
+
+puts "Name any allegries one at a time. Write 'done' when done."
+allergies = gets.chomp 
+
+until allergies == "sunshine" || allergies == "done"
+
+
+allergies = gets.chomp
+ 
 end 
 
-if age == (current_year - year_born) && (garlic_bread || health_insurance = true)
+
+  
+
+
+surveys_taken += 1
+
+
+if age == (current_year - year_born) && (garlic_bread == "yes" || health_insurance == "yes")
     puts "probably not a vampire"
 
-elsif age != (current_year - year_born) && (garlic_bread || health_insurance = false)
+elsif age != (current_year - year_born) && (garlic_bread == "no" && health_insurance == "yes") || (garlic_bread == "yes" && health_insurance == "no") || allergies == "sunshine"
     puts "probably a vampire"
+   
 
-elsif age != (current_year - year_born) && garlic_bread = false && health_insurance = false
+elsif age != (current_year - year_born) && garlic_bread == "no" && health_insurance == "no"
   puts "Almost certainly a vampire"
 
-elsif name == "Drake Cula" || "Tu Fang"
+elsif name == "Drake Cula" || name == "Tu Fang"
   puts "Definitely a vampire"
 
 else 
   puts "Results inconclusive"
 end 
+
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
+
+
+  
+
+
+surveys_taken += 1
+
+
+if age == (current_year - year_born) && (garlic_bread == "yes" || health_insurance == "yes")
+    puts "probably not a vampire"
+
+elsif age != (current_year - year_born) && (garlic_bread == "no" && health_insurance == "yes") || (garlic_bread == "yes" && health_insurance == "no") || allergies == "sunshine"
+    puts "probably a vampire"
+   
+
+elsif age != (current_year - year_born) && garlic_bread == "no" && health_insurance == "no"
+  puts "Almost certainly a vampire"
+
+elsif name == "Drake Cula" || name == "Tu Fang"
+  puts "Definitely a vampire"
+
+else 
+  puts "Results inconclusive"
+end 
+
+end
+
+puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
 
   
