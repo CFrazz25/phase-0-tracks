480768380ecfbcdc20c0f4328875b6920c539ff0
Christopher Frazzini <cfrazzin@indiana.edu>
2017-01-03 18:07:34 -0600

    started release 2 updates

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,19 +2,48 @@ puts "what is your name?"
 name = gets.chomp
 
 puts "how old are you?"
-age = gets.chomp
+age = gets.chomp.to_i
 
 puts "what year were you born?"
-year_born = gets.chomp
+year_born = gets.chomp.to_i
+current_year = 2016
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (yes/no)"
 garlic_bread = gets.chomp
 
-puts "Would you like to enroll in the company’s health insurance?"
+if garlic_bread == "yes"
+  garlic_bread = true
+elsif garlic_bread == "no"
+  garlic_bread = false
+end 
+    
+
+puts "Would you like to enroll in the company’s health insurance? (yes/no)"
 health_insurance = gets.chomp 
 
+if health_insurance == "yes"
+  health_insurance = true
+elsif health_insurance == "no"
+  health_insurance = false
+end 
 
-wolves_like_sunshine = true
-wolves_like_garlic = true
-vampires_like_sunshine = false
-vampires_like_garlic = false
+if age == (current_year - year_born) && (garlic_bread || health_insurance = true)
+    puts "probably not a vampire"
+
+elsif age != (current_year - year_born) && (garlic_bread || health_insurance = false)
+    puts "probably a vampire"
+
+elsif age != (current_year - year_born) && garlic_bread = false && health_insurance = false
+  puts "Almost certainly a vampire"
+
+elsif name == "Drake Cula" || "Tu Fang"
+  puts "Definitely a vampire"
+
+else 
+  puts "Results inconclusive"
+end 
+
+
+  
+  
+      
