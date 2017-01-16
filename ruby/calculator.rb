490693a9bd5669d0bf67d123b9d0bb96dfdad354
Christopher Frazzini <cfrazzin@indiana.edu>
2017-01-15 22:10:32 -0600

    added user interface

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -3,7 +3,19 @@ def calculate(integer1, operator, integer2)
   end 
 
 #Driver Code
-puts calculate(4 , "+", 5)
-puts calculate(3, "*", 3)
-puts calculate(10, "-", 2)
-puts calculate(10, "/", 2)
+ #puts calculate(4 , "+", 5)
+ #puts calculate(3, "*", 3)
+ #puts calculate(10, "-", 2)
+ #puts calculate(10, "/", 2)
+ 
+ puts "add a calculation!"
+calculation = gets.chomp 
+calculation.split("")
+
+
+
+integer1 = calculation[0].to_i
+operator = calculation[2].to_s
+integer2 = calculation[4].to_i
+
+puts calculate(integer1, operator, integer2)
