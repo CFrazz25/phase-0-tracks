8dbcc5478c1ed5923f5b588c8beda1f7e9b4e9d7
Christopher Frazzini <cfrazzin@indiana.edu>
2017-01-15 22:56:23 -0600

    made updates to allow numbers with 2 digits work

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -3,23 +3,27 @@ def calculate(integer1, operator, integer2)
   end 
 
 #Driver Code
- #puts calculate(4 , "+", 5)
+ #puts calculate(40 , "+", 50)
  #puts calculate(3, "*", 3)
  #puts calculate(10, "-", 2)
  #puts calculate(10, "/", 2)
  
+  all_calculations = []
  puts "add a calculation! type 'done' to exit"
 calculation = gets.chomp 
-calculation.split("")
+new_calc = calculation.split(" ")
 
 until calculation == "done"
-  
-integer1 = calculation[0].to_i
-operator = calculation[2].to_s
-integer2 = calculation[4].to_i
 
-puts calculate(integer1, operator, integer2)
+integer1 = new_calc[0].to_i
+operator = new_calc[1].to_s
+integer2 = new_calc[2].to_i
+p calculate(integer1, operator, integer2)
+all_calculations.push "#{integer1} #{operator} #{integer2} = #{calculate(integer1, operator, integer2)}"
 puts "add another calculation if you want!"
 calculation = gets.chomp 
-calculation.split("")
+new_calc = calculation.split(" ")
 end 
+
+puts "#{all_calculations.length} calculations performend: 
+      #{all_calculations}"
