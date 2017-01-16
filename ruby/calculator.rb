c40d8696939c4afae298edc1252c3b55c8d2875e
Christopher Frazzini <cfrazzin@indiana.edu>
2017-01-15 22:14:38 -0600

    completed question #4

--- ruby/calculator.rb
+++ ruby/calculator.rb
@@ -8,14 +8,18 @@ def calculate(integer1, operator, integer2)
  #puts calculate(10, "-", 2)
  #puts calculate(10, "/", 2)
  
- puts "add a calculation!"
+ puts "add a calculation! type 'done' to exit"
 calculation = gets.chomp 
 calculation.split("")
 
-
-
+until calculation == "done"
+  
 integer1 = calculation[0].to_i
 operator = calculation[2].to_s
 integer2 = calculation[4].to_i
 
 puts calculate(integer1, operator, integer2)
+puts "add another calculation if you want!"
+calculation = gets.chomp 
+calculation.split("")
+end 
