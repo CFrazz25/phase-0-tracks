245111f428f7e13472a6eee7a1a81d49da2237fb
Christopher Frazzini <cfrazzin@indiana.edu>
2017-01-16 21:21:14 -0600

    made puppy class behaviors and charateristics

---
+++ ruby/puppy.rb
@@ -0,0 +1,19 @@
+=begin 
+Class: Puppies
+
+Characteristics: 
+Legs: 4
+Cuddle buddies: yes
+color: varies
+name: varies
+Fur type: varies
+For petting: yes
+
+
+Behavior:
+Cuddles
+runs
+pees and poops whenever, where ever.
+Barking, crying 
+
+=end 
