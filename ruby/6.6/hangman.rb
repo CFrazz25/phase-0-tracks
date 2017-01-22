362c89b790a315cb5bb5cd870f83835932a389b2
Christopher Frazzini <cfrazzin@indiana.edu>
2017-01-22 15:46:31 -0600

    made final adjustments in test code and main program file to work with testing

--- ruby/6.6/hangman.rb
+++ ruby/6.6/hangman.rb
@@ -1,22 +1,26 @@
 class Hangman
   
   def initialize
-     
+     puts "creating game!"
   end
-  
+
+# This is where the first user will create a word for their friend to guess, 
+# and when they create the word, it will result in empty spaces of how long the word is,
+# showcasing how many letters are in the word.  
   def create_word(word)
     @word = word
     puts "Cool, but this will stay hidden. ;-)"
     puts "your friend will get #{@word.length} guesses. Repeat guesses, will not count against you."
     p @secret_letters = ["_ " * @word.length]
+
   end 
   
-  def guessed_letters(letter)
-    @letter = letter
-  end
     
-  
-  def show_progress(all_guesses)
+# This is where we will show the progress of the word from how many and what letters the 
+# friend has guessed so far. If they get a letter correct in the word, that letter will show up,
+# if they get a letter wrong, the empty space in the secret word will stay empty.   
+  def show_progress(all_guesses, word)
+    @word = word
      secret_word = "" 
      @word.chars.each do |letter|
        if all_guesses.include?(letter)
@@ -28,7 +32,10 @@ class Hangman
     return secret_word
   end 
 
-  def guess_word(guess)
+# Once the friend uses up all their guesses, they will get one shot at guessing the word. 
+# If they get the word corret, it will let them know they won, if not, the program will taunt them. 
+  def guess_word(guess, word)
+    @word = word
     if guess == @word
       puts "you win!"
     else 
@@ -38,30 +45,33 @@ class Hangman
 end 
   
   
-# Driver code 
+# DRIVER CODE!!! 
 # Ask the user to put in a word for their friend to guess. 
-game = Hangman.new 
 puts "Please create a word for your friend to guess."
 word = gets.chomp 
+game = Hangman.new
+
+# The game will create 
 game.create_word(word)
 
 
-# Here the user guesses the letters in the word. 
+# Here the user guesses the letters in the word and will keep track of all guesses made.
 all_guesses = []
 number_of_guesses = word.length
 new_guesses = []
+
 until new_guesses.length == number_of_guesses
   puts "guess a letter"
   guessed_letter = gets.chomp 
-  game.guessed_letters(guessed_letter)
   all_guesses << guessed_letter
   puts "Here's what you have guessed so far:"
   puts all_guesses
+
 #below will make sure repeat guesses are not counted against the user 
   new_guesses = all_guesses.uniq
   puts "you have #{word.length - new_guesses.length} guesses left!"
 
-  p game.show_progress(all_guesses)
+  p game.show_progress(all_guesses, word)
 
 end 
 puts "that's all the guesses you get."
@@ -69,4 +79,4 @@ puts "that's all the guesses you get."
 # The user's friend can guess the word after all of their attempts are over. 
 puts "guess word"
 guess = gets.chomp 
-game.guess_word(guess)
+game.guess_word(guess, word)
--- ruby/6.6/hangman_spec.rb
+++ ruby/6.6/hangman_spec.rb
@@ -4,23 +4,19 @@ describe Hangman do
   let(:game) { Hangman.new }
 
   it "adds word to game for friend to guess" do
-    game.create_word("baseball")
-    expect(game.create_word).to eq ["baseball"]
+    
+    expect(game.create_word("baseball")).to eq ["_ _ _ _ _ _ _ _ "]
   end
 
-  it "a letter guess to the mystery word" do
-    game.guessed_letter("b")
-    expect(game.guessed_letter).to eq ["b"]
+it "shows progress made for correct letter" do
+    
+    expect(game.show_progress("baseball", "baseball")).to eq "baseball"
   end
-
-  it "shows progress made for correct letter" do
-    game.show_progress("b")
-    expect(game.show_progress).to eq ["b _ _ _ _ _ _ _"]
-  end
+  
 
   it "tells you if you got the mystery word correct or not" do
-    game.guess_word("baseball")
-    expect(game.create_word).to eq ["you win!"]
+     
+    expect(game.guess_word("baseball", "baseball")).to eq "you win!"
   end
 
 
