706fab5bfd9ea7b09aa45a3898417373eaad2826
Christopher Frazzini <cfrazzin@indiana.edu>
2017-01-26 23:22:23 -0600

    updated 7.2

--- js/data_stuctures.js
+++ js/data_stuctures.js
@@ -1,10 +1,11 @@
+//release 0
 var colors = ["blue", "orange", "green", "yellow"];
 var names = ["Edward", "Chris", "Christina", "Tabitha"];
 
 colors.push("black");
 names.push("Henry");
 
-
+//release 1
 thing = {};
 for(var i = 0; i < colors.length; i++){
   thing[names[i]] = colors[i];
@@ -51,3 +52,25 @@ var ourThirdCar = new Car("Tesla", 1874, false);
 console.log(ourThirdCar.model);
 console.log("This new car cannot race....");
 console.log("-~-~-")
+
+
+//release 3
+// 1. If you wanted to loop through the keys and values of an object, how would you do that? 
+// (There are a few ways to accomplish this, and some gotchas that can happen depending on your approach.)
+
+// You can use a for...in loop. You then need to use a .hasOwnPropery function in an if statement inside the loop
+// looking for the key-value pair. Can also use a .each function similar to ruby, but with diff. syntax.
+// Here's a good resource I found: https://coderwall.com/p/_kakfa/javascript-iterate-through-object-keys-and-values
+
+
+// 2. Are there advantages to using constructor functions to create objects? 
+// Disadvantages? 
+
+// If you want to have multiple instances, it'd be better to use constructor vs literal functions. Also,
+// if you want to add behavior, it's better to use constructor.
+// Here was a good resource where I reviewed this question: http://stackoverflow.com/questions/4859800/should-i-be-using-object-literals-or-constructor-functions
+
+
+
+
+
