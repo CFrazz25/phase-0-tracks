c455395b998bbd4bfcfb438962ae68fed7100221
Christopher Frazzini <cfrazzin@indiana.edu>
2017-02-05 14:59:39 -0600

    made iteration to add gamblers to table

--- databases/8.5/8_5.rb
+++ databases/8.5/8_5.rb
@@ -14,7 +14,7 @@ db.execute(create_table_cmd)
 
 # method to gamble 
 def gambling (name, bet)
-  money_left = bet
+  $money_left = bet
   cards_1 = ["A", "K", "Q", "J", 10, 9, 8, 7, 6, 5, 4, 3, 2]
 
   x = 0 
@@ -28,33 +28,46 @@ def gambling (name, bet)
     p results = [card1, card2, card3, card4, card5]
     solutions = results.uniq
       if solutions.count < 3 
-        money_left += 3
+        $money_left += 3
         puts "you're pretty lucky with that 4-pair"
       elsif solutions.count < 4 
-        money_left += 2
+        $money_left += 2
         puts "you got three of a kind or two pairs!"
       elsif solutions.count < 5 
-        money_left += 1
+        $money_left += 1
         puts "you got a pair!"
       else
-        money_left -= 1
+        $money_left -= 1
         puts "sorry, nothing"
       end 
     solutions.count
     x += 1 
-    p "you have $#{money_left} now."
+    p "you have $#{$money_left} now."
   end 
 end
 #end of gambling method 
 
 puts "What's your name?"
-name = gets.chomp
+$name = gets.chomp
+name = $name
+names = []
+names << name 
 
 puts "how many times do you want to bet? $1 a bet."
 bet = gets.chomp.to_i
 gambling(name, bet)
-
+money_left = $money_left
 
 def create_gambler(db, name, money_left)
   db.execute("INSERT INTO gamblers (name, money_left) VALUES (?, ?)", [name, money_left])
 end
+
+names.each do 
+  create_gambler(db, name, money_left)
+end 
+
+
+
+
+
+
