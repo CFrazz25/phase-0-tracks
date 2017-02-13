13e016a5643b64a97bfb52fb29a3df208e0c5ffd
Christopher Frazzini <cfrazzin@indiana.edu>
2017-02-12 22:23:10 -0600

    testing a little jquery and more research

---
+++ web_dev/jquery/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('button').click(function(){
+        $('.red').fadeOut();
+        $('.blue').fadeOut();
+        $('.green').hide();
+        $('.yellow').fadeOut();
+        $('.pink').fadeOut();
+    
+        
+    });
+});
